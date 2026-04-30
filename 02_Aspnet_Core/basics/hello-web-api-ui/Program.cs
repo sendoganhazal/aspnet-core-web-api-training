38ed7b9e61ad1ce5b480276fcac75910e09786d2
Hazal Sendogan <sendoganhazal@gmail.com>
2026-04-30 15:51:27 +0300

    projenin incelenmesi

--- 02_Aspnet_Core/basics/hello-web-api-ui/Program.cs
+++ 02_Aspnet_Core/basics/hello-web-api-ui/Program.cs
@@ -1,16 +1,20 @@
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args); // Bize web application üzerinden bir Builder verir. 
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(); // 1. servis kaydý
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer(); // 2. servis kaydý
+builder.Services.AddSwaggerGen( ); // 3. servis kaydý
 
 var app = builder.Build();
+/* 
+ * builder'a Build fonk. üzerinden çalýþtýrdýðýmýzda bize bir web application döner.
+ * Bu webapp (app deðiþkeni) kullanabiliriz
+ */
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if ( app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
@@ -22,4 +26,4 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+app.Run(); // uygulamanýn koþmasýný saðlar

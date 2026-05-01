1b974d60c1a4c8a99582fe3b9c7558b8680a4f60
Hazal Sendogan <sendoganhazal@gmail.com>
2026-05-01 16:27:30 +0300

    get requests

--- bookDemo/bookDemo/Controllers/BooksController.cs
+++ bookDemo/bookDemo/Controllers/BooksController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using bookDemo.Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace bookDemo.Controllers
@@ -7,5 +8,23 @@ namespace bookDemo.Controllers
     [ApiController]
     public class BooksController : ControllerBase
     {
+        [HttpGet]
+        public IActionResult GetAllBooks()
+        {
+            var books = ApplicationContext.Books;
+            return Ok(books);
+        }
+        [HttpGet("{id:int}")]
+        public IActionResult GetOneBook([FromRoute(Name = "id")] int id)
+        {
+            var book = ApplicationContext.Books.Where(b => b.Id.Equals(id)).SingleOrDefault();
+
+            if(book is null) 
+            {
+                return NotFound(); // 404 döner
+            }
+
+            return Ok(book);
+        }
     }
 }
--- bookDemo/bookDemo/obj/Debug/net8.0/bookDemo.AssemblyInfo.cs
+++ bookDemo/bookDemo/obj/Debug/net8.0/bookDemo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bookDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c6f3963bd669441422bc3387e974dab256592aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a02e40419c3ec2850c9644d3a001275ae54f7100")]
 [assembly: System.Reflection.AssemblyProductAttribute("bookDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bookDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

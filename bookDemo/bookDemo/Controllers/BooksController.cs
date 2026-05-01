6c6f3963bd669441422bc3387e974dab256592aa
Hazal Sendogan <sendoganhazal@gmail.com>
2026-05-01 15:46:18 +0300

    project created

---
+++ bookDemo/bookDemo/Controllers/BooksController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bookDemo.Controllers
+{
+    [Route ( "api/books" )]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+    }
+}

bb733dab0aa262c79ad7f932d67a0aab3764cb86
Hazal Sendogan <sendoganhazal@gmail.com>
2026-05-01 14:13:41 +0300

    configuration

--- basics/ProductApp/obj/Debug/net8.0/ProductApp.AssemblyInfo.cs
+++ basics/ProductApp/obj/Debug/net8.0/ProductApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e769b80fecc8a84ca2f4a72ce768c7e8044225cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff6ff11ab8bbe0a1d201807753c5a6bac54c05c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

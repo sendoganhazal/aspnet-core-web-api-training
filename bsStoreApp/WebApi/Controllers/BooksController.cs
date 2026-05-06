faf46bbf28eb1b31d3166359e8b110749f0e9f69
Hazal Sendogan <sendoganhazal@gmail.com>
2026-05-06 16:24:24 +0300

    data manipulation

--- bsStoreApp/WebApi/Controllers/BooksController.cs
+++ bsStoreApp/WebApi/Controllers/BooksController.cs
@@ -10,5 +10,114 @@ namespace WebApi.Controllers
     [ApiController]
     public class BooksController : ControllerBase
     {
+        private readonly RepositoryContext _context;
+
+        public BooksController(RepositoryContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult getAllBooks()
+        {
+            try
+            {
+                var books = _context.Books.ToList();
+                return Ok(books);
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+       
+        }
+        [HttpGet("{id:int}")]
+        public IActionResult GetOneBook([FromRoute (Name = "id")] int id)
+        {
+            try
+            {
+                var book = _context.Books.Where(b => b.Id.Equals(id)).SingleOrDefault();
+
+                if (book is null)
+                {
+                    return NotFound(); // 404 
+                }
+
+                return Ok(book);
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+     
+        }
+        [HttpPost]
+        public IActionResult CreateOneBook([FromBody] Book book)
+        {
+            try
+            {
+                if(book is null)
+                {
+                    return BadRequest(); // 400
+                }
+                _context.Books.Add(book);
+                _context.SaveChanges();
+                return StatusCode(201, book);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex);
+            }
+
+        }
+
+        [HttpPut("{id:int}")]
+        public IActionResult UpdateOneBook([FromRoute(Name = "id")] int id, [FromBody] Book book)
+        {
+            try
+            {
+                // check book
+                var entity = _context.Books.Where(b => b.Id.Equals(id)).SingleOrDefault();
+                if (entity is null)
+                {
+                    return NotFound();
+                }
+                // check id
+                if (id != book.Id)
+                {
+                    return BadRequest("Parametre Hatası");
+                }
+                entity.Title = book.Title;
+                entity.Price = book.Price;
+
+                _context.SaveChanges();
+                return Ok (book);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex);
+            }
+        }
+        [HttpDelete ("{id:int}")]
+        public IActionResult DeleteOneBook([FromRoute(Name = "id")] int id)
+        {
+            // check book
+            var entity = _context.Books.Where(b => b.Id.Equals(id)).SingleOrDefault();
+            if (entity is null)
+            {
+                return NotFound (new
+                {
+                    statusCode = 404,
+                    message = $"Book with id: {id} could not found"
+                });// 404
+            }
+
+            _context.Books.Remove(entity);
+            _context.SaveChanges();
+            return NoContent(); // 204
+        }
     }
 }
--- bsStoreApp/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ bsStoreApp/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18f0a912c8e23141b1c1cd70ea3ccf3f6e0d8cf1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cebcf2566d4191f733ea25befdce8fe1cc4d844")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

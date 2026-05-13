99a5d9c7e47a174e175df5a9056544e7547abc7c
Hazal Sendogan <sendoganhazal@gmail.com>
2026-05-13 13:22:43 +0300

    IRepositoryBase

--- bsStoreApp/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ bsStoreApp/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2041eefdfc8771dd1294b4b43026dde58799c19d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b325c4968e40b03951152ad568fa72c1dcdeca07")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

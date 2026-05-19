25d52a065f6e9781cf6eca1b2a2c2714bb0723bf
Hazal Sendogan <sendoganhazal@gmail.com>
2026-05-19 12:59:26 +0300

    IBookService

--- bsStoreApp/Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
+++ bsStoreApp/Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e658e6130c0ecf5a9f1e156ab1bd94c54de99ab9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d400f6907b9901fbd276d14671bfaf6f58e154a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- bsStoreApp/Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
+++ bsStoreApp/Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4abf763eaa1884df9da15bc2070ae19b1482160f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d400f6907b9901fbd276d14671bfaf6f58e154a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

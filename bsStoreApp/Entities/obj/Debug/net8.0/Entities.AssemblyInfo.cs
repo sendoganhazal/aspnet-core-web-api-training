780929eb248dcac9a68bb4f81c9c1b58f09d3127
Hazal Sendogan <sendoganhazal@gmail.com>
2026-05-19 16:10:58 +0300

    repository context factory

--- bsStoreApp/Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
+++ bsStoreApp/Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d400f6907b9901fbd276d14671bfaf6f58e154a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+913daaf755591d58434f47eeeb0dbed94e101525")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

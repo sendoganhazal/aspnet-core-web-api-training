d065a040e51f5897ba4dd7ae732cbeaa9520198d
Hazal Sendogan <sendoganhazal@gmail.com>
2026-05-19 16:13:25 +0300

    after merge

--- bsStoreApp/Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
+++ bsStoreApp/Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+913daaf755591d58434f47eeeb0dbed94e101525")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ce882c5a8f5b7d349de5f60cc8da8125528f081")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

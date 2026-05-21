9419be7b44b31b3bb98efa297ab5c9f078ddef13
Hazal Sendogan <sendoganhazal@gmail.com>
2026-05-21 17:10:21 +0300

    dto and mapping profile

--- bsStoreApp/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ bsStoreApp/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1892b29e324cda013322eaa312bd0faa024f9f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8216f5627ad322d7f0d23080d0a2a7235364a211")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

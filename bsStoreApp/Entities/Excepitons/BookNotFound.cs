d0e4c9c88da01f0cc35bd90f56d38f21e8fb2f2f
Hazal Sendogan <sendoganhazal@gmail.com>
2026-05-21 16:13:03 +0300

    use exception handling

--- bsStoreApp/Entities/Excepitons/BookNotFound.cs
+++ bsStoreApp/Entities/Excepitons/BookNotFound.cs
@@ -1,6 +1,6 @@
 ﻿namespace Entities.Excepitons
 {
-    public sealed class BookNotFound : NotFound
+    public sealed class BookNotFound : NotFoundException
     {
         public BookNotFound ( int id ) : base ( $"The book with id {id} does not exist in the database." )
         {

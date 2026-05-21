fc38e483931f00ab1c159871aa5360bd0bfe3a66
Hazal Sendogan <sendoganhazal@gmail.com>
2026-05-21 15:44:09 +0300

    try catch removed

--- bsStoreApp/Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
+++ bsStoreApp/Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab39be7c7b73a83a9b72aedb74752fa507501d62")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e4194afbc6e35cae31367793672425bf28e88cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- bsStoreApp/Presentation/Controllers/BooksController.cs
+++ bsStoreApp/Presentation/Controllers/BooksController.cs
@@ -11,125 +11,84 @@ using System.Threading.Tasks;
 namespace Presentation.Controllers
 {
     [ApiController]
-    [Route("api/books")]
+    [Route ( "api/books" )]
     public class BooksController : ControllerBase
     {
         private readonly IServiceManager _manager;
-        public BooksController(IServiceManager manager)
+        public BooksController ( IServiceManager manager )
         {
             _manager = manager;
         }
 
         [HttpGet]
-        public IActionResult GetAllBooks()
+        public IActionResult GetAllBooks ( )
         {
-            try
-            {
-                var books = _manager.BookService.GetAllBooks(false);
-                return Ok(books);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+
+            var books = _manager.BookService.GetAllBooks(false);
+            return Ok ( books );
         }
 
-        [HttpGet("{id:int}")]
-        public IActionResult GetOneBook([FromRoute(Name = "id")] int id)
+        [HttpGet ( "{id:int}" )]
+        public IActionResult GetOneBook ( [FromRoute ( Name = "id" )] int id )
         {
-            try
-            {
-                throw new Exception("This is a test exception for global error handling.");
-                var book = _manager
+            throw new Exception ( "This is a test exception for global error handling." );
+            var book = _manager
                 .BookService
                 .GetOneBookById(id, false);
 
-                if (book is null)
-                    return NotFound(); //404
+            if ( book is null )
+                return NotFound ( ); //404
 
-                return Ok(book);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+            return Ok ( book );
 
         }
 
         [HttpPost]
-        public IActionResult CreateOneBook([FromBody] Book book)
+        public IActionResult CreateOneBook ( [FromBody] Book book )
         {
-            try
-            {
-                if (book is null)
-                    return BadRequest(); // 400 
+            if ( book is null )
+                return BadRequest ( ); // 400 
 
-                _manager.BookService.CreateOneBook(book);
+            _manager.BookService.CreateOneBook ( book );
 
-                return StatusCode(201, book);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
+            return StatusCode ( 201, book );
         }
 
-        [HttpPut("{id:int}")]
-        public IActionResult UpdateOneBook([FromRoute(Name = "id")] int id,
-            [FromBody] Book book)
+        [HttpPut ( "{id:int}" )]
+        public IActionResult UpdateOneBook ( [FromRoute ( Name = "id" )] int id,
+            [FromBody] Book book )
         {
-            try
-            {
-                if (book is null)
-                    return BadRequest(); // 400 
+            if ( book is null )
+                return BadRequest ( ); // 400 
 
-                _manager.BookService.UpdateOneBook(id, book, true);
-                return NoContent(); // 204
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+            _manager.BookService.UpdateOneBook ( id, book, true );
+            return NoContent ( ); // 204
         }
 
-        [HttpDelete("{id:int}")]
-        public IActionResult DeleteOneBook([FromRoute(Name = "id")] int id)
+        [HttpDelete ( "{id:int}" )]
+        public IActionResult DeleteOneBook ( [FromRoute ( Name = "id" )] int id )
         {
-            try
-            {
-                _manager.BookService.DeleteOneBook(id, false);
-                return NoContent();
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+            _manager.BookService.DeleteOneBook ( id, false );
+            return NoContent ( );
         }
 
 
-        [HttpPatch("{id:int}")]
-        public IActionResult PartiallyUpdateOneBook([FromRoute(Name = "id")] int id,
-            [FromBody] JsonPatchDocument<Book> bookPatch)
+        [HttpPatch ( "{id:int}" )]
+        public IActionResult PartiallyUpdateOneBook ( [FromRoute ( Name = "id" )] int id,
+            [FromBody] JsonPatchDocument<Book> bookPatch )
         {
-            try
-            {
-                // check entity
-                var entity = _manager
+            // check entity
+            var entity = _manager
                     .BookService
                     .GetOneBookById(id, true);
 
-                if (entity is null)
-                    return NotFound(); // 404
+            if ( entity is null )
+                return NotFound ( ); // 404
 
-                bookPatch.ApplyTo(entity);
-                _manager.BookService.UpdateOneBook(id, entity, true);
+            bookPatch.ApplyTo ( entity );
+            _manager.BookService.UpdateOneBook ( id, entity, true );
 
-                return NoContent(); // 204
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(ex.Message);
-            }
+            return NoContent ( ); // 204
         }
     }
 }

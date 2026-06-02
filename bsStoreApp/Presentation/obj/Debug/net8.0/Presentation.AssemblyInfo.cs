28907abcd1478f1ace126c5f241fcec556968ff0
Hazal Sendogan <sendoganhazal@gmail.com>
2026-06-02 11:18:33 +0300

    before merge

--- bsStoreApp/Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
+++ bsStoreApp/Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40f93969c8260cb5aa7681f756c4e3dd8b1727ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a01bcd35623fd66659a325a2bb0dca5c805f45f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- bsStoreApp/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ bsStoreApp/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40f93969c8260cb5aa7681f756c4e3dd8b1727ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a01bcd35623fd66659a325a2bb0dca5c805f45f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

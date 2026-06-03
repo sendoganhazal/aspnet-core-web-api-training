e4e16f96c5c459ab936855bf9f7da78c4c86a17c
Hazal Sendogan <sendoganhazal@gmail.com>
2026-06-03 13:15:27 +0300

    request parameters

---
+++ bsStoreApp/Entities/RequestFeatures/RequestParameters.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.RequestFeatures
+{
+    public abstract class RequestParameters
+    {
+        const int maxPageSize = 50;
+
+        // auto-implemented properties
+        public int PageNumber { get; set; }
+
+        // Full property with a backing field
+        private int _pageSize;
+
+        public int PageSize
+        { 
+            get { return _pageSize; } 
+            set { _pageSize = value > maxPageSize ? maxPageSize : value; }
+        }
+    }
+}
--- bsStoreApp/Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
+++ bsStoreApp/Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97109653cd0f8726054cba04765d2752bf99d7f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+934ce4c96887be8155dbce5ac815797c0f0a8134")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

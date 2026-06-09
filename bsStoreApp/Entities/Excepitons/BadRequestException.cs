faf71d2688c607890feb237a201e1ab5a800e39b
Hazal Sendogan <sendoganhazal@gmail.com>
2026-06-09 15:14:28 +0300

    bad request exception

---
+++ bsStoreApp/Entities/Excepitons/BadRequestException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Excepitons
+{
+    public abstract class BadRequestException : Exception
+    {
+        protected BadRequestException ( string? message ) : base ( message )
+        {
+        }
+    }
+}
---
+++ bsStoreApp/Entities/Excepitons/PriceOutofRangeBadRequestException.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities.Excepitons
+{
+    public class PriceOutofRangeBadRequestException : BadRequestException
+    {
+        public PriceOutofRangeBadRequestException ( ) : base ( "Maximum price should be less than 1000 and greater than 10." )
+        {
+        }
+    }
+}

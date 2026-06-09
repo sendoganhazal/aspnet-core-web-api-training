efae6ea09a3cad3982e06c371f61882deceb4d14
Hazal Sendogan <sendoganhazal@gmail.com>
2026-06-09 16:31:41 +0300

    search

---
+++ bsStoreApp/Repositories/Extensions/BooksRepositoryExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Extensions
+{
+    public static class BooksRepositoryExtensions
+    {
+        public static IQueryable<Book> FilterBooks ( this IQueryable<Book> books,
+            uint minPrice, uint maxPrice ) =>
+            books.Where ( b => b.Price >= minPrice && b.Price <= maxPrice );
+        public static IQueryable<Book> Search ( this IQueryable<Book> books,
+            string? searchTerm )
+        {
+            if ( string.IsNullOrWhiteSpace ( searchTerm ) )
+                return books;
+            var lowerCaseSearchTerm = searchTerm.Trim ( ).ToLower ( ); // BlAcK -> black
+            return books.Where ( b => b.Title.ToLower ( ).Contains ( lowerCaseSearchTerm ) );
+        }
+    }
+}
--- bsStoreApp/Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
+++ bsStoreApp/Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b082ce9d1ee04e7665b81967c8cc4c8ad053870c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98050e6623253b254b7e0eab2e0217a6effc7737")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
